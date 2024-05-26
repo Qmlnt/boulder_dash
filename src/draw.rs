f65b8c8939f4f53584e52acdc314c0742f7a0f39
Qmlnt <qmelint@gmail.com>
2024-05-26 12:37:40 +0300

    refactor the logic a bit

--- src/draw.rs
+++ src/draw.rs
@@ -1,41 +1,57 @@
 use console::Term;
-use std::io;
+use std::error::Error;
 
-use super::level;
-use level::Obj;
+use crate::{
+    args::AppMode,
+    level::{Obj, State, Update},
+    Config,
+};
 
-pub fn tui(level: &level::Update) -> io::Result<()> {
-    let out = Term::stdout();
+pub fn draw(level: &Update, config: &Config) -> Result<(), Box<dyn Error>> {
+    match config.app_mode {
+        AppMode::Tui => {
+            let out = Term::stdout();
 
-    out.clear_screen()?;
-    out.hide_cursor()?;
+            out.clear_screen()?;
+            out.hide_cursor()?;
 
-    for row in level.matrix {
-        let mut line = String::new();
-        for obj in row {
-            line.push(obj.char());
+            for row in level.matrix {
+                let mut line = String::new();
+                for obj in row {
+                    line.push(obj.char());
+                }
+                out.write_line(&line)?;
+            }
+
+            let status_msg = match level.state {
+                Some(State::Win) => "You have won!".to_string(),
+                Some(State::Lose) => "You have lost!".to_string(),
+                None => format!(
+                    "\nScore: {}/{}\nDelay: {}ms\nPaused: {}",
+                    level.score,
+                    level.max_score,
+                    config.delay,
+                    if config.pause { "yes" } else { "no" }
+                ),
+            };
+
+            out.write_line(&status_msg)?;
         }
-        out.write_line(&line)?;
+        AppMode::Gui => {
+            todo!()
+        }
     }
-
-    let status_msg = match level.state {
-        Some(level::State::Win) => "You have won!".to_string(),
-        Some(level::State::Lose) => "You have lost!".to_string(),
-        None => format!("\nScore: {}/{}", level.score, level.max_score),
-    };
-
-    out.write_line(&status_msg)?;
-
-    // out.move_cursor_down(999)?; // bottom
-    // out.write_line(&lines)?;
-    // out.move_cursor_up(1)?;
-    // out.move_cursor_to(0, 0)?;
-    // out.move_cursor_to(x, y)?;
-    // out.write_line(obj.map_or(" ", |o| o.char()))?;
 
     Ok(())
 }
 
-// pub fn gui(level: &Update) {
-//     todo!();
+// pub fn tui(level: &level::Update, delay: u16, paused: bool) -> io::Result<()> {
+// out.move_cursor_down(999)?; // bottom
+// out.write_line(&lines)?;
+// out.move_cursor_up(1)?;
+// out.move_cursor_to(0, 0)?;
+// out.move_cursor_to(x, y)?;
+// out.write_line(obj.map_or(" ", |o| o.char()))?;
+
+// Ok(())
 // }
--- src/level/objects/player.rs
+++ src/level/objects/player.rs
@@ -15,29 +15,30 @@ impl Obj for Player {
 
         // to prevent the rock from falling on player when object underneath is broken
         let mut player_broke = false;
+        let mut above_point = Dir::Up.apply_to(&level.player);
 
         // Player
         if let Some(dir) = direction {
             let next_point = dir.apply_to(&level.player);
 
             player_broke = level.get_obj(next_point).breakable();
-            let move_next = matches!(dir, Dir::Left | Dir::Right)
+            let can_move_next = matches!(dir, Dir::Left | Dir::Right)
                 && level.get_obj(next_point).rock()
                 && level.get_obj(dir.apply_to(&next_point)).void();
 
             if player_broke {
                 requests.extend(level.get_obj(next_point).on_broken(level));
-            } else if move_next {
+            } else if can_move_next {
                 requests.push(Request::MoveObj(next_point, dir.apply_to(&next_point)));
             }
 
-            if level.get_obj(next_point).void() || player_broke || move_next {
+            if level.get_obj(next_point).void() || player_broke || can_move_next {
                 requests.push(Request::MoveObj(level.player, next_point));
+                above_point = Dir::Up.apply_to(&next_point);
             }
         }
 
         // Check the rock above
-        let above_point = Dir::Up.apply_to(&level.player);
         if !player_broke && level.get_obj(above_point).rock() {
             requests.push(Request::UpdateState(State::Lose));
             requests.push(Request::MoveObj(above_point, level.player));
